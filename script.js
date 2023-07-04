c969cca2a80397f904aafcb303e2ca432c15d56e
jenstem <124745888+jenstem@users.noreply.github.com>
2023-07-04 14:04:37 -0400

    Remove to do js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ setInterval(updateTime, 1000);
 
 
 
-// TODO: Add code to apply the past, present, or future class to each time
+
 // block by comparing the id to the current hour. HINTS: How can the id
 // attribute of each time-block be used to conditionally add or remove the
 // past, present, and future classes?
