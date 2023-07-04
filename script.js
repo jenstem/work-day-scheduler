2745b028f88d716a8ed9816a1a8606f579ad7196
jenstem <124745888+jenstem@users.noreply.github.com>
2023-07-04 11:08:54 -0400

    Revise js storage areas

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ var saveBtn = $('.saveBtn');
 var currentTime = dayjs();
 
 // where should this go? This isn't getting called
-function stopBrowserDefault(e) {
-  e.preventDefault();
-}
+// function stopBrowserDefault(e) {
+//   e.preventDefault();
+// }
 
 function updateTime() {
   currentTime = dayjs();
@@ -22,7 +22,7 @@ function updateTime() {
   currentDay.text(formattedTime);
 }
 
-stopBrowserDefault();
+// stopBrowserDefault();
 
 updateTime();
 
@@ -76,26 +76,42 @@ $(".saveBtn").on("click", function() {
   var textValue = siblingTextArea.val();
   var hourID = thisBtn.parent().attr('id');
   localStorage.setItem(hourID, textValue);
-  console.log("saved");
 })
 
 // values saved in local storage, need to print to DOM
 $('.time-block').each(function(i) {
-  $(id .description).val(localStorage.getItem(id));
+  // $(id .description).val(localStorage.getItem(id));
   // for each time-block element, use the id to grab the related value from localstorage
   // and set it as the text of the child <textarea>
   // assign the value from local storage using getItem to a variable with textContent attached to the
   // element of textarea
-  // $("hour-9 .description").val(localStorage.getItem("hour-9"));
-  // $("hour-10 .description").val(localStorage.getItem("hour-10"));
-  // $("hour-11 .description").val(localStorage.getItem("hour-11"));
-  // $("hour-12 .description").val(localStorage.getItem("hour-12"));
-  // $("hour-13 .description").val(localStorage.getItem("hour-13"));
-  // $("hour-14 .description").val(localStorage.getItem("hour-14"));
-  // $("hour-15 .description").val(localStorage.getItem("hour-15"));
-  // $("hour-16 .description").val(localStorage.getItem("hour-16"));
-  // $("hour-17 .description").val(localStorage.getItem("hour-17"));
-  // $("#hour-09"+i).children("textarea").val(blockStorage)
+  var storeStorage9 = localStorage.getItem("hour-9");
+  var storeStorage10 = localStorage.getItem("hour-10");
+  var storeStorage11 = localStorage.getItem("hour-11");
+  var storeStorage12 = localStorage.getItem("hour-12");
+  var storeStorage13 = localStorage.getItem("hour-13");
+  var storeStorage14 = localStorage.getItem("hour-14");
+  var storeStorage15 = localStorage.getItem("hour-15");
+  var storeStorage16 = localStorage.getItem("hour-16");
+  var storeStorage17 = localStorage.getItem("hour-17");
+  $("hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("hour-17 .description").val(localStorage.getItem("hour-17"));
+  $("#hour-9").children("textarea").val(storeStorage9);
+  $("#hour-10").children("textarea").val(storeStorage10);
+  $("#hour-11").children("textarea").val(storeStorage11);
+  $("#hour-12").children("textarea").val(storeStorage12);
+  $("#hour-13").children("textarea").val(storeStorage13);
+  $("#hour-14").children("textarea").val(storeStorage14);
+  $("#hour-15").children("textarea").val(storeStorage15);
+  $("#hour-16").children("textarea").val(storeStorage16);
+  $("#hour-17").children("textarea").val(storeStorage17);
 })
 
 });
