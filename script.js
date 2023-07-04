653091ca85c3c3de1bb4dff04adf759b915239f6
jenstem <124745888+jenstem@users.noreply.github.com>
2023-07-04 14:04:21 -0400

    Remove comment js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ setInterval(updateTime, 1000);
 
 
 
-//
+
 // TODO: Add code to apply the past, present, or future class to each time
 // block by comparing the id to the current hour. HINTS: How can the id
 // attribute of each time-block be used to conditionally add or remove the
