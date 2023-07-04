936c074976061b6ed021d8da15e2e4924ee2228c
jenstem <124745888+jenstem@users.noreply.github.com>
2023-07-04 14:07:14 -0400

    Remove to do js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ setInterval(updateTime, 1000);
 
 
 
-// TODO: Add code to display the current date in the header of the page. X
+
 
 
 // needs work - flicks on for a moment - only change class for a split second
